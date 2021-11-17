2543397d609bdc15fa15fc04ea1a011db81e1fa4
Yadeli Lopez <ingrid_yadeli@hotmail.com>
2021-11-17 11:32:22 -0600

    actualización

--- Logeo/login.js
+++ Logeo/login.js
@@ -7,20 +7,18 @@ BotonValidar.addEventListener("click", function () {
     params.append("usu", usuario);
     params.append("pass", password);
     console.log("{" + "name:'" + params.get("usu") + "'," + "pass:'" + params.get("pass") + "'}");
-    //var params2 = "{" + "name:'" + params.get("nombre") + "'," + "pass:'" + params.get("pass") + "'}";  // forma de equívoca de construir el objeto, ya que le sobran las llaves
     var params3 =  "name:'" + params.get("usu") + "'," + "pass:'" + params.get("pass") + "'";
     axios.post("http://localhost:4567/holaJson", { "usu": params.get("usu"), pass: params.get("pass") } )
         .then(function (rs) {
             console.log(rs.data);
-            //alert(rs.data)
         })
         .catch(function (error) {
             console.log(error);
         });
 
-        if(params.get("usu") == "Nadia"){
-            if(params.get("pass") == "12345"){
-                console.log("Hola");
+        if(params.get("usu") == "yayis"){
+            if(params.get("pass") == "cata29"){
+                console.log("Bienvenido");
                 window.location.replace('usuarios.html');
             }
         }else{
